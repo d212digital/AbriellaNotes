fff8bce16ed246d6fe23dece81542111ad651a59
David Adamson <david@212digital.co.uk>
2022-09-05 10:03:19 +0100

    added new notes and new notbook methods in NotesVM and bindingin GUI

--- AbriellaNotes/ViewModel/NotesVM.cs
+++ AbriellaNotes/ViewModel/NotesVM.cs
@@ -4,61 +4,104 @@ using AbriellaNotes.ViewModel.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AbriellaNotes.ViewModel
 {
-    public class NotesVM
+    public class NotesVM : INotifyPropertyChanged
     {
         public ObservableCollection<Notebook> Notebooks { get; set; }
-		
-		private Notebook selecteNotebook;
-		public Notebook SelectedNotebook
-		{
-			get { return selecteNotebook; }
-			set 
-			{ 
-				selecteNotebook = value; 
-				//TODO: get notes
-			}
-		}
 
-		public ObservableCollection<Note> Notes { get; set; }
-		public NewNotebookCommand NewNotebookCommand { get; set; }
-		public NewNoteCommand NewNoteCommand { get; set; }
+        private Notebook selecteNotebook;
 
-		public NotesVM()
-		{
-			NewNotebookCommand = new NewNotebookCommand(this);
-			NewNoteCommand = new NewNoteCommand(this);
-		}
-
-		public void CreateNewNotebook()
-		{
-			Notebook newNotebook = new Notebook()
-			{
-				Name = "New notebook"
-			};
-
-            DatabaseHelper.Insert(newNotebook);
+        public Notebook SelectedNotebook
+        {
+            get { return selecteNotebook; }
+            set
+            {
+                selecteNotebook = value;
+                OnPropertyChanged("SelectedNotebook");
+                GetNotes();
+            }
         }
 
-		public void CreateNote(int notebookId)
-		{
-			Note newNote = new Note()
-			{
-				NotebookId = notebookId,
-				CreatedAt = DateTime.Now,
-				UpdatedAt = DateTime.Now,
-				Title = "New Note"
+        public ObservableCollection<Note> Notes { get; set; }
+        public NewNotebookCommand NewNotebookCommand { get; set; }
+        public NewNoteCommand NewNoteCommand { get; set; }
 
-			};
+        public event PropertyChangedEventHandler PropertyChanged;
 
-			DatabaseHelper.Insert(newNote);
+        public NotesVM()
+        {
+            NewNotebookCommand = new NewNotebookCommand(this);
+            NewNoteCommand = new NewNoteCommand(this);
 
-		}
+            Notebooks = new ObservableCollection<Notebook>();
+            Notes = new ObservableCollection<Note>();
 
-	}
+            GetNotebooks();
+        }
+
+        public void CreateNewNotebook()
+        {
+            Notebook newNotebook = new Notebook()
+            {
+                Name = "New notebook"
+            };
+
+            DatabaseHelper.Insert(newNotebook);
+
+            GetNotebooks();
+        }
+
+        public void CreateNote(int notebookId)
+        {
+            Note newNote = new Note()
+            {
+                NotebookId = notebookId,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+                Title = "New Note"
+
+            };
+
+            DatabaseHelper.Insert(newNote);
+
+            GetNotes();
+
+        }
+
+        private void GetNotebooks()
+        {
+            var notebooks = DatabaseHelper.Read<Notebook>();
+
+            Notebooks.Clear();
+            foreach (var notebook in notebooks)
+            {
+                Notebooks.Add(notebook);
+            }
+        }
+        private void GetNotes()
+        {
+            if (SelectedNotebook != null)
+            {
+                var notes = DatabaseHelper.Read<Note>().Where(n => n.NotebookId == SelectedNotebook.Id).ToList();
+
+                Notes.Clear();
+                foreach (var note in notes)
+                {
+                    Notes.Add(note);
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
 }
