fa5e06d15c2bce85fde7e7a193a4faf90477bf3d
David Adamson <david@212digital.co.uk>
2022-09-05 07:02:59 +0100

    added notebook and notes commands

---
+++ AbriellaNotes/ViewModel/NotesVM.cs
@@ -0,0 +1,29 @@
+﻿using AbriellaNotes.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbriellaNotes.ViewModel
+{
+    public class NotesVM
+    {
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+		
+		private Notebook selecteNotebook;
+		public Notebook SelectedNotebook
+		{
+			get { return selecteNotebook; }
+			set 
+			{ 
+				selecteNotebook = value; 
+				//TODO: get notes
+			}
+		}
+
+		public ObservableCollection<Note> Notes { get; set; }
+
+	}
+}
