acfcace06e932113d67afe9f84d0aaa06aa7ddfd
David Adamson <david@212digital.co.uk>
2022-09-07 15:36:43 +0100

    added user controls for displaying notes and notebooks

--- AbriellaNotes/View/UserControls/DisplayNotebookControl.xaml.cs
+++ AbriellaNotes/View/UserControls/DisplayNotebookControl.xaml.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using AbriellaNotes.Model;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,6 +19,26 @@ namespace AbriellaNotes.View.UserControls
     /// </summary>
     public partial class DisplayNotebookControl : UserControl
     {
+        public Notebook Notebook
+        {
+            get { return (Notebook)GetValue(NotebookProperty); }
+            set { SetValue(NotebookProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NotebookProperty =
+            DependencyProperty.Register("Notebook", typeof(Notebook), typeof(DisplayNotebookControl), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DisplayNotebookControl notebookUserControl = d as DisplayNotebookControl;
+
+            if (notebookUserControl != null)
+            {
+                notebookUserControl.DataContext = notebookUserControl.Notebook;
+            }
+        }
+
         public DisplayNotebookControl()
         {
             InitializeComponent();
