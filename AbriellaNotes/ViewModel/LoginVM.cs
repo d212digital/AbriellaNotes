f2b46d938fe39090390b4f2573be0ac378a6cdae
David Adamson <david@212digital.co.uk>
2022-09-21 10:32:31 +0100

    adding user login screens

--- AbriellaNotes/ViewModel/LoginVM.cs
+++ AbriellaNotes/ViewModel/LoginVM.cs
@@ -2,14 +2,18 @@
 using AbriellaNotes.ViewModel.Commands;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace AbriellaNotes.ViewModel
 {
-    public class LoginVM
+    public class LoginVM : INotifyPropertyChanged
     {
+		private bool isShowingRegister = false;
+        
 		private User user;
 
 		public User User
@@ -18,15 +22,66 @@ namespace AbriellaNotes.ViewModel
 			set { user = value; }
 		}
 
-		public RegisterCommand RegisterCommand { get; set; }
+		private Visibility loginVis;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public Visibility LoginVis
+		{
+			get { return loginVis; }
+			set
+			{
+				loginVis = value;
+				OnPropertyChanged("LoginVis");
+			}
+		}
+        private Visibility registerVis;
+		public Visibility RegisterVis
+        {
+            get { return registerVis; }
+            set
+            {
+                registerVis = value;
+                OnPropertyChanged("RegisterVis");
+            }
+           
+        }
+        public RegisterCommand RegisterCommand { get; set; }
 
 		public LoginCommand LoginCommand { get; set; }
 
-		public LoginVM()
+        public ShowRegisterCommand ShowRegisterCommand { get; set; }
+
+        public LoginVM()
 		{
+			LoginVis = Visibility.Visible;
+			registerVis = Visibility.Collapsed;
+            
 			RegisterCommand = new RegisterCommand(this);
 			LoginCommand = new LoginCommand(this);
-		}
+            ShowRegisterCommand = new ShowRegisterCommand(this);
+        }
 
-	}
+        public void SwitchViews()
+        {
+            isShowingRegister = !isShowingRegister;
+
+            if (isShowingRegister)
+            {
+                RegisterVis = Visibility.Visible;
+                LoginVis = Visibility.Collapsed;
+            }
+            else
+            {
+                RegisterVis = Visibility.Collapsed;
+                LoginVis = Visibility.Visible; 
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
 }
